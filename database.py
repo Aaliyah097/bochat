061fa42e81eb31ae59031b2301ce0f57b80ce245
Aaliyah097 <name.boltz@gmail.com>
2024-05-11 17:14:01 +0300

    S

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import databases
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.pool.impl import AsyncAdaptedQueuePool
+from sqlalchemy.pool.impl import AsyncAdaptedQueuePool, NullPool
 from config import settings
 
 
@@ -11,10 +11,10 @@ Base = declarative_base()
 
 engine = create_async_engine(
     settings.postgres_conn_string,
-    pool_size=50,
-    max_overflow=50,
+    # pool_size=50,
+    # max_overflow=50,
     echo=False,
-    poolclass=AsyncAdaptedQueuePool,
+    poolclass=NullPool,
     isolation_level="READ COMMITTED",
 )
 
