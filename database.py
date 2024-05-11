0af267d56b538f1e61fe87bed74b1a7183217336
Aaliyah097 <name.boltz@gmail.com>
2024-05-11 15:03:20 +0300

    fix

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import databases
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.pool.impl import AsyncAdaptedQueuePool
+from sqlalchemy.pool import NullPool
 from config import settings
 
 
@@ -12,11 +13,8 @@ Base = declarative_base()
 engine = create_async_engine(
     settings.postgres_conn_string,
     pool_size=250,
-    echo=False,
-    pool_pre_ping=True,
-    poolclass=AsyncAdaptedQueuePool,
-    pool_recycle=7200,
-    isolation_level="READ COMMITTED"
+    echo=True,
+    poolclass=NullPool,
 )
 
 SessionFactory = sessionmaker(autocommit=False,
