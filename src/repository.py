0a03fb38695cf5d8da0fbb9262ba8c5820872042
Aaliyah097 <name.boltz@gmail.com>
2024-05-17 08:40:14 +0300

    убрана постгрес

--- src/repository.py
+++ src/repository.py
@@ -2,15 +2,10 @@ from typing import TypeVar, Generic, Any, Tuple, Type, Dict
 import datetime
 import pytz
 from pydantic import BaseModel
-from sqlalchemy.orm import sessionmaker
 from mongo_database import MongoDBClient
 
 
-DBO = TypeVar('DBO', bound=Base)
-DTO = TypeVar('DTO', bound=BaseModel)
-
-
-class Repository(Generic[DBO, DTO]):
+class Repository:
     messages_collection = "messages"
     lights_collection = "lights"
 
@@ -21,18 +16,3 @@ class Repository(Generic[DBO, DTO]):
     def get_now() -> datetime.datetime:
         return datetime.datetime.now().astimezone(
             pytz.timezone('Europe/Moscow')).now()
-
-    @staticmethod
-    def struct_from_tuple(keys: Tuple[str, ...], values: Tuple[Any, ...]) -> Dict:
-        return dict(zip(keys, values))
-
-    @staticmethod
-    def dto_from_struct(struct: Dict, dto_cls: Type[DTO]) -> DTO:
-        return dto_cls.model_validate(struct)
-
-    @staticmethod
-    def dto_from_dbo(dbo: DBO, dto_cls: Type[DTO]) -> DTO:
-        payload = {}
-        for column in dbo.__table__.columns:
-            payload[column.name] = getattr(dbo, column.name)
-        return dto_cls.model_validate(payload)
